11b079035c4263a3f6f7822d15821209d8e42e62
rwiiin <rwiiinzakeri@gmail.com>
2025-06-04 17:18:09 +0330

    work on it

--- src/components/HeroOne.tsx
+++ src/components/HeroOne.tsx
@@ -6,135 +6,137 @@ import Image from "next/image";
 import { FaArrowDown } from "react-icons/fa6";
 
 const HeroOne = () => {
-    return (   <>
-        <div
-          className="absolute  inset-0 -z-30 opacity-5 "
-          style={{ backgroundImage: `url(${grainImage.src})` }}
-        ></div>
-        <div className="p-8 md:py-48 relative z-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)] overflow-hidden pb-28">
-          <div className="size-[620px] circle"></div>
-          <div className="size-[820px] circle"></div>
-          <div className="size-[1020px] circle"></div>
-          <div className="size-[1220px] circle"></div>
-          <HeroOrbit size={800} rotate={-72}>
-            <Image className="size-28" src={Star} alt="star" color="white" />
-          </HeroOrbit>
-          <HeroOrbit size={550} rotate={20}>
-            <Image className="size-12" src={Star} alt="star" color="white" />
-          </HeroOrbit>
-          <HeroOrbit size={590} rotate={98}>
-            <Image className="size-8" src={Star} alt="star" color="white" />
-          </HeroOrbit>
-          <HeroOrbit size={430} rotate={98}>
-            <Image className="size-8" src={Star} alt="star" color="white" />
-          </HeroOrbit>
-          <HeroOrbit size={430} rotate={-14}>
-            <Image
-              className="size-8 opacity-50"
-              src={Star}
-              alt="star"
-              color="white"
-            />
-          </HeroOrbit>
-  
-          <HeroOrbit size={440} rotate={90}>
-            <Image
-              className="size-5 opacity-50"
-              src={Star}
-              alt="star"
-              color="white"
-            />
-          </HeroOrbit>
-          <HeroOrbit size={530} rotate={178}>
-            <Image
-              className="size-8 opacity-50"
-              src={Star}
-              alt="star"
-              color="white"
-            />
-          </HeroOrbit>
-          <HeroOrbit size={530} rotate={144}>
-            <Image
-              className="size-14 opacity-50"
-              src={Star}
-              alt="star"
-              color="white"
-            />
-          </HeroOrbit>
-          <HeroOrbit size={720} rotate={0}>
-            <Image
-              className="size-14 opacity-50"
-              src={Star}
-              alt="star"
-              color="white"
-            />
-          </HeroOrbit>
-          <HeroOrbit size={720} rotate={90}>
-            <Image
-              className="size-14 opacity-50"
-              src={Star}
-              alt="star"
-              color="white"
-            />
-          </HeroOrbit>
-          <HeroOrbit size={720} rotate={180}>
-            <Image
-              className="size-14 opacity-50"
-              src={Star}
-              alt="star"
-              color="white"
-            />
-          </HeroOrbit>
-          <HeroOrbit size={720} rotate={-90}>
-            <Image
-              className="size-14 opacity-50"
-              src={Star}
-              alt="star"
-              color="white"
-            />
-          </HeroOrbit>
-          <HeroOrbit size={650} rotate={27}>
-            <Image
-              className="size-14 opacity-50"
-              src={Star}
-              alt="star"
-              color="white"
-            />
-          </HeroOrbit>
-          <div className="flex flex-col items-center">
-            <div>
-              <Image className="size-[100px]" src={memojiImage} alt="computer" />
+  return (
+    <>
+      <div
+        className="absolute  inset-0 -z-30 opacity-5 "
+        style={{ backgroundImage: `url(${grainImage.src})` }}
+      ></div>
+      <div className="p-8 md:py-48 relative z-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)] overflow-hidden pb-28">
+        <div className="size-[620px] circle"></div>
+        <div className="size-[820px] circle"></div>
+        <div className="size-[1020px] circle"></div>
+        <div className="size-[1220px] circle"></div>
+        <HeroOrbit size={800} rotate={-72}>
+          <Image className="size-28" src={Star} alt="star" color="white" />
+        </HeroOrbit>
+        <HeroOrbit size={550} rotate={20}>
+          <Image className="size-12" src={Star} alt="star" color="white" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotate={98}>
+          <Image className="size-8" src={Star} alt="star" color="white" />
+        </HeroOrbit>
+        <HeroOrbit size={430} rotate={98}>
+          <Image className="size-8" src={Star} alt="star" color="white" />
+        </HeroOrbit>
+        <HeroOrbit size={430} rotate={-14}>
+          <Image
+            className="size-8 opacity-50"
+            src={Star}
+            alt="star"
+            color="white"
+          />
+        </HeroOrbit>
+
+        <HeroOrbit size={440} rotate={90}>
+          <Image
+            className="size-5 opacity-50"
+            src={Star}
+            alt="star"
+            color="white"
+          />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotate={178}>
+          <Image
+            className="size-8 opacity-50"
+            src={Star}
+            alt="star"
+            color="white"
+          />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotate={144}>
+          <Image
+            className="size-14 opacity-50"
+            src={Star}
+            alt="star"
+            color="white"
+          />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotate={0}>
+          <Image
+            className="size-14 opacity-50"
+            src={Star}
+            alt="star"
+            color="white"
+          />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotate={90}>
+          <Image
+            className="size-14 opacity-50"
+            src={Star}
+            alt="star"
+            color="white"
+          />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotate={180}>
+          <Image
+            className="size-14 opacity-50"
+            src={Star}
+            alt="star"
+            color="white"
+          />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotate={-90}>
+          <Image
+            className="size-14 opacity-50"
+            src={Star}
+            alt="star"
+            color="white"
+          />
+        </HeroOrbit>
+        <HeroOrbit size={650} rotate={27}>
+          <Image
+            className="size-14 opacity-50"
+            src={Star}
+            alt="star"
+            color="white"
+          />
+        </HeroOrbit>
+        <div className="flex flex-col items-center relative z-50">
+          <div>
+            <Image className="size-[100px]" src={memojiImage} alt="computer" />
+          </div>
+          <div className="bg-gray-950 px-4 py-1.5 border border-gray-800 flex items-center gap-2 rounded-lg">
+            <div className="size-2.5 bg-green-500 rounded-full"></div>
+            <div className="size-2.5 bg-green-500 rounded-full animate-ping absolute"></div>
+            <div className="text-sm font-medium">
+              Available for new projects
             </div>
-            <div className="bg-gray-950 px-4 py-1.5 border border-gray-800 flex items-center gap-2 rounded-lg">
-              <div className="size-2.5 bg-green-500 rounded-full"></div>
-              <div className="size-2.5 bg-green-500 rounded-full animate-ping absolute"></div>
-              <div className="text-sm font-medium">
-                Available for new projects
-              </div>
-            </div>
-            <div className="max-w-xl mx-auto">
-              <h1 className="font-serif md:font-bold md:text-5xl text-3xl text-center mt-8">
-                Building Exceptional User Experiences
-              </h1>
-              <p className="text-center mt-4 text-white/60">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-                atque iusto nihil earum voluptas animi itaque consectetur,
-                quisquam magni
-              </p>
-              <div className="flex flex-col mt-8 gap-4 md:flex-row items-center justify-center mx-auto ">
-                <button className="w-fit  inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl font-semibold">
-                  <span>Explore My Work</span>
-                  <FaArrowDown className="text-white" />
-                </button>
-                <button className="inline-flex items-center gap-2 border border-white rounded-xl bg-white h-12 px-6 text-gray-900 w-fit  ">
-                  <span>👋</span>
-                  <span className="font-semibold">Lets Connet</span>
-                </button>
-              </div>
+          </div>
+          <div className="max-w-xl mx-auto">
+            <h1 className="font-serif md:font-bold md:text-5xl text-3xl text-center mt-8">
+              Building Exceptional User Experiences
+            </h1>
+            <p className="text-center mt-4 text-white/60">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+              atque iusto nihil earum voluptas animi itaque consectetur,
+              quisquam magni
+            </p>
+            <div className="flex flex-col mt-8 gap-4 md:flex-row items-center justify-center mx-auto ">
+              <button className="w-fit  inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl font-semibold">
+                <span>Explore My Work</span>
+                <FaArrowDown className="text-white" />
+              </button>
+              <button className="inline-flex items-center gap-2 border border-white rounded-xl bg-white h-12 px-6 text-gray-900 w-fit  ">
+                <span>👋</span>
+                <span className="font-semibold">Lets Connet</span>
+              </button>
             </div>
           </div>
         </div>
-      </> );
-}
- 
+      </div>
+    </>
+  );
+};
+
 export default HeroOne;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,9 @@
+import ArrowUp from "@/assets/icons/arrow-up-right.svg";
 import CheckIcon from "@/assets/icons/check-circle.svg";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
+import grainImage from "@/assets/images/grain.jpg";
+
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import Image from "next/image";
 
@@ -45,53 +48,76 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <div>
-      <div className="container">
+    <section className="pb-16 lg:py-24">
+      <div className="container mx-auto">
         <div className="flex justify-center">
           <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300  to-sky-400 text-transparent bg-clip-text text-center">
             Real-world Results
           </p>
         </div>
-        <h2 className="font-serif text-3xl text-center mt-6">
+        <h2 className="font-serif text-3xl text-center mt-6 md:text-3xl font-bold">
           Feature Projects
         </h2>
-        <p className="mt-4 text-white/60">
+        <p className="mt-4 text-white/60 max-w-md mx-auto text-center">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Exercitationem ratione excepturi assumenda enim cumque
         </p>
-        <div className="flex flex-col mt-10">
+        <div className="flex flex-col mt-10 justify-center w-full gap-20 md:mt-20">
           {portfolioProjects.map((project) => (
             <div
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2  after:outline-white/20 after:-outline-offset-2 after:rounded-3xl p-8"
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2  after:outline-white/20 after:-outline-offset-2 after:rounded-3xl px-8 pt-8 lg:flex justify-between lg:gap-4"
             >
               <div>
-                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm gap-2 text-transparent bg-clip-text ">
-                  <span>
-                    {project.company} {project.year}
-                  </span>
-                  <span>&bull;</span>
-                  <span>{project.year}</span>
+                <div
+                  className="absolute inset-0 -z-10 opacity-5 bg-red-300"
+                  style={{ backgroundImage: `url(${grainImage.src})` }}
+                ></div>
+
+                <div>
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm gap-2 text-transparent bg-clip-text ">
+                    <span>
+                      {project.company} {project.year}
+                    </span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
                 </div>
+                <h3 className="font-serif text-2xl mt-2 md:text-4xl font-bold">
+                  {project.title}
+                </h3>
+                <hr className="border-t-2 md:text-3xl border-white/5 mt-4 md:mt-5" />
+                <ul className="flex flex-col gap-4 mt-4">
+                  {project.results.map((result) => (
+                    <li
+                      key={result.title}
+                      className="flex gap-2 text-sm text-white/50"
+                    >
+                      <Image
+                        className="w-5 md:w-6"
+                        src={CheckIcon}
+                        alt="CheckIcon"
+                      />
+                      {result.title}
+                    </li>
+                  ))}
+                </ul>
+                <a href={project.link}>
+                  <button className="flex items-center justify-center gap-2 bg-white text-gray-950 h-12 w-full rounded-xl font-semibold mt-8 md:w-fit md:px-3">
+                    <span>View Live Site</span>
+                    <Image className="w-5" src={ArrowUp} alt="CheckIcon" />
+                  </button>
+                </a>
               </div>
-              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
-              <hr className="border-t-2 border-white/5 mt-4" />
-              <ul>
-                {project.results.map((result) => (
-                  <li key={result.title}>
-                    <Image src={CheckIcon} alt="CheckIcon" />
-                    {result.title}
-                  </li>
-                ))}
-              </ul>
-              <a href={project.link}>
-                <button>View Live Site</button>
-              </a>
-              <Image src={project.image} alt={project.title} />
+              <Image
+                className="mt-8 -mb-4"
+                src={project.image}
+                alt={project.title}
+              />
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
